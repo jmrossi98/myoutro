441ff050caebb2c516fe31860884c36b5c6aba92
jmrossi98 <jmrossi98@gmail.com>
2022-09-10 17:07:48 -0400

    Clean up code, clicking home closes menu

--- src/outroapp.py
+++ src/outroapp.py
@@ -10,50 +10,58 @@ class OutroApp(QtWidgets.QMainWindow):
         self.menu_open = False
 
         self.toolButton.clicked.connect(lambda: self.slide_menu())
+        self.toolButton_2.clicked.connect(lambda: self.open_home())
+
+    def close_menu(self):
+        self.open_animation = QtCore.QPropertyAnimation(self.frame, b"maximumWidth")
+        self.open_animation.setDuration(500)
+        self.open_animation.setStartValue(150)
+        self.open_animation.setEndValue(50)
+        self.open_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.open_animation.start()
+        self.close_animation = QtCore.QPropertyAnimation(self.frame, b"minimumWidth")
+        self.close_animation.setDuration(500)
+        self.close_animation.setStartValue(150)
+        self.close_animation.setEndValue(50)
+        self.close_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.close_animation.start()
+
+        menu_icon = QtGui.QIcon(os.path.join(self.dir_path, 'media', 'icons', 'icons8-menu.svg'))
+        self.toolButton.setIcon(menu_icon)
+        self.menu_open = False
+
+    def open_menu(self):
+        self.open_animation = QtCore.QPropertyAnimation(self.frame, b"maximumWidth")
+        self.open_animation.setDuration(500)
+        self.open_animation.setStartValue(50)
+        self.open_animation.setEndValue(150)
+        self.open_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.open_animation.start()
+        self.close_animation = QtCore.QPropertyAnimation(self.frame, b"minimumWidth")
+        self.close_animation.setDuration(500)
+        self.close_animation.setStartValue(50)
+        self.close_animation.setEndValue(150)
+        self.close_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.close_animation.start()
+
+        close_icon = QtGui.QIcon(os.path.join(self.dir_path, 'media', 'icons', 'icons8-close.svg'))
+        self.toolButton.setIcon(close_icon)
+        self.menu_open = True
 
     def slide_menu(self):
         if not self.menu_open:
-            self.open_animation = QtCore.QPropertyAnimation(self.frame, b"maximumWidth")
-            self.open_animation.setDuration(500)
-            self.open_animation.setStartValue(50)
-            self.open_animation.setEndValue(150)
-            self.open_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
-            self.open_animation.start()
-
-            self.close_animation = QtCore.QPropertyAnimation(self.frame, b"minimumWidth")
-            self.close_animation.setDuration(500)
-            self.close_animation.setStartValue(50)
-            self.close_animation.setEndValue(150)
-            self.close_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
-            self.close_animation.start()
-
-            close_icon = QtGui.QIcon(os.path.join(self.dir_path, 'media', 'icons', 'icons8-close.svg'))
-            self.toolButton.setIcon(close_icon)
-            self.menu_open = True
-
+            self.open_menu()
         else:
-            self.open_animation = QtCore.QPropertyAnimation(self.frame, b"maximumWidth")
-            self.open_animation.setDuration(500)
-            self.open_animation.setStartValue(150)
-            self.open_animation.setEndValue(50)
-            self.open_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
-            self.open_animation.start()
+            self.close_menu()
 
-            self.close_animation = QtCore.QPropertyAnimation(self.frame, b"minimumWidth")
-            self.close_animation.setDuration(500)
-            self.close_animation.setStartValue(150)
-            self.close_animation.setEndValue(50)
-            self.close_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
-            self.close_animation.start()
-
-            menu_icon = QtGui.QIcon(os.path.join(self.dir_path, 'media', 'icons', 'icons8-menu.svg'))
-            self.toolButton.setIcon(menu_icon)
-            self.menu_open = False
-
+    def open_home(self):
+        if self.menu_open:
+            self.close_menu()
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainwindow = OutroApp()
+    mainwindow.setWindowTitle("MyOutro")
     mainwindow.show()
     sys.exit(app.exec_())
